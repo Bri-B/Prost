3c0d5747f34b6f328f2d9d0814c055ebedc45a86
Bri-b <bri.skinner02@gmail.com>
2020-11-15 13:15:02 -0600

    (fix) capitalization EContact to Econtact

--- src/components/Customer/Profile/Home.jsx
+++ src/components/Customer/Profile/Home.jsx
@@ -1,41 +1,43 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { Grid, Typography, Button } from '@material-ui/core'
-import Logout from '../../Logout.jsx'
+import React from 'react';
+import { Grid, Typography, Button } from '@material-ui/core';
+import Logout from '../../Logout.jsx';
 
-export default function Home({ setView, name, setViewValue, img }) {
-
-    return (
-        <Grid container direction="column" justify="center" alignItems="center">
-            <Grid item container direction="row" justify="center" alignItems="center">
-                <img src={img || "https://i.imgur.com/jRnsxbB.png"} style={{ height: '100px', width: '100px' }} />
-            </Grid>
-            <Grid item container direction="row" justify="center" alignItems="center">
-                <Typography variant="subtitle1">
-                    @{name}
-                </Typography>
-            </Grid>
-            <Button variant="outlined" color="primary" onClick={() => setView('Favorite')}>
-                Favorite Spots
+export default function Home({
+  setView, name, setViewValue, img,
+}) {
+  return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Grid item container direction="row" justify="center" alignItems="center">
+        <img src={img || 'https://i.imgur.com/jRnsxbB.png'} style={{ height: '100px', width: '100px' }} />
+      </Grid>
+      <Grid item container direction="row" justify="center" alignItems="center">
+        <Typography variant="subtitle1">
+          @
+          {name}
+        </Typography>
+      </Grid>
+      <Button variant="outlined" color="primary" onClick={() => setView('Favorite')}>
+        Favorite Spots
       </Button>
-            <Button variant="outlined" color="primary" onClick={() => setView('EContact')}>
-                Emergency Contact
+      <Button variant="outlined" color="primary" onClick={() => setView('Econtact')}>
+        Emergency Contact
       </Button>
-            <Button variant="outlined" color="primary" onClick={() => setView('Ride')}>
-                Call a Ride
+      <Button variant="outlined" color="primary" onClick={() => setView('Ride')}>
+        Call a Ride
       </Button>
-            <Button variant="outlined" color="primary" onClick={() => setView('Checkin')}>
-                Check in
+      <Button variant="outlined" color="primary" onClick={() => setView('Checkin')}>
+        Check in
       </Button>
-            <Button variant="outlined" color="primary" onClick={() => setView('History')}>
-                History
+      <Button variant="outlined" color="primary" onClick={() => setView('History')}>
+        History
       </Button>
-            <Button variant="outlined" color="primary" onClick={() => setView('Translate')}>
-                Translate
+      <Button variant="outlined" color="primary" onClick={() => setView('Translate')}>
+        Translate
       </Button>
-            <Button variant="outlined" color="primary" onClick={() => setView('Friend')}>
-                Friend's List
+      <Button variant="outlined" color="primary" onClick={() => setView('Friend')}>
+        Friend's List
       </Button>
-            <Logout setViewValue={setViewValue} />
-        </Grid>
-    )
+      <Logout setViewValue={setViewValue} />
+    </Grid>
+  );
 }
